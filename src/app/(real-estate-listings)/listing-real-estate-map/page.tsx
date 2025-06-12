088719b7849c38ad93099a67728f313f42d87cc4
aseran74 <alvaroserr@gmail.com>
2025-06-13 01:20:59 +0200

    Eliminar referencias a SectionGridHasMap en pages para evitar errores de compilación

--- src/app/(real-estate-listings)/listing-real-estate-map/page.tsx
+++ src/app/(real-estate-listings)/listing-real-estate-map/page.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
-import SectionGridHasMap from "../SectionGridHasMap";
+// import SectionGridHasMap from "../SectionGridHasMap";
 
 export interface ListingRealEstateMapPageProps {}
 
 const ListingRealEstateMapPage: FC<ListingRealEstateMapPageProps> = ({}) => {
   return (
     <div className="container pb-24 lg:pb-28 2xl:pl-10 xl:pr-0 xl:max-w-none">
-      <SectionGridHasMap />
+      {/* <SectionGridHasMap /> */}
     </div>
   );
 };
--- src/app/(stay-listings)/listing-stay-map/page.tsx
+++ src/app/(stay-listings)/listing-stay-map/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React, { FC } from "react";
-import SectionGridHasMap from "../SectionGridHasMap";
+// import SectionGridHasMap from "../SectionGridHasMap";
 
 export interface ListingStayMapPageProps {}
 
 const ListingStayMapPage: FC<ListingStayMapPageProps> = ({}) => {
   return (
     <div className="container pb-24 lg:pb-28 2xl:pl-10 xl:pr-0 xl:max-w-none">
-      <SectionGridHasMap />
+      {/* <SectionGridHasMap /> */}
     </div>
   );
 };
